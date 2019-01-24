fbe9a6df085091aeb9fce6417c4eaf44e6df264e
Francesco Corradini <francesco.corradini@24consulting.it>
2019-01-24 15:43:21 +0100

    rimosso test

--- index.js
+++ index.js
@@ -197,7 +197,6 @@ module.exports = (api, options) => {
   }
 
   const runBuild = async (platform, args) => {
-    console.log(args)
     // add cordova.js, define process.env.CORDOVA_PLATFORM
     chainWebPack(platform)
     // set build output folder
